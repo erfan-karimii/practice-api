ca5fc046c245419af09e15cb37d98424b96e6f5f
erfan-karimi <h410mi3@gmail.com>
2024-04-04 04:48:51 +0330

    add drf_spectacular to project

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'snippets',
     'product',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [
@@ -128,5 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    
 }
